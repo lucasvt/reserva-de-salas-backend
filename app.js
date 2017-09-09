3f2afd8458e3027e09e8e8b9776666a95a506162
Lucas Vaz Tassoni <lucasvt2@gmail.com>
2017-09-09 01:32:16 -0300

    Instalação e configuração do MongoDB
    - Criada configuração do banco de dados MongoDB utilizando o Mongoose;
    - Criado schema Reservas;
    - Instalado e configurado o Body Parser para conversão de dados para JSON;
    - Criado rota do tipo post para salvar os dados no bd;

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
 var express = require('express');
-
 var cors = require('cors');
+var bodyParser = require('body-parser');
+
+//Importa o mongoose e o conecta com o mongodb
+var mongoose = require('mongoose').connect('mongodb://127.0.0.1/reserva_de_salas');
+var db = mongoose.connection;
+
 
 var app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended : true
+}));
 
+db.on('error', console.error.bind(console, 'Erro ao conectar o banco'));
+
+db.once('open', function () {
+    //Schema de Reservas
+    Reserva = mongoose.model('Reserva', mongoose.Schema({
+        local : Object,
+        sala : String,
+        dataInicio : Date,
+        dataFim : Date,
+        responsavel : String,
+        cafe : Boolean,
+        descricao : String
+    }));
+});
 
 
 app.get('/', function (req, res) {
-    res.end('Servidor ON!');
+
+
 });
 
 app.get('/reservas', function (req, res) {
@@ -62,5 +86,31 @@ app.get('/salas', function (req, res) {
     ]);
 });
 
+app.post('/reservas', function (req, res) {
+    new Reserva({
+            local : req.body.local,
+            sala : req.body.sala,
+            dataInicio : req.body.dataInicio,
+            dataFim : req.body.dataFim,
+            responsavel : req.body.responsavel,
+            cafe : req.body.cafe,
+            descricao : req.body.descricao
+        }).save(function (error, reserva) {
+            if(error){
+                res.json({error: 'Não foi pocivel salvar a reserva'})
+            }else{
+                res.json(reserva);
+            }
+        });
+   console.log(req.body);
+});
+
+app.put('/reservas', function (req, res) {
+
+});
+
+app.delete('/reservas', function (req, res) {
+
+});
 
 app.listen(5000);
